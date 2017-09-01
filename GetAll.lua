3e2c73fa0052dfb04abca944726d2a3e13511f33
call23re <the23re@gmail.com>
2017-09-01 15:04:38 -0500

    Update and rename Recursion.lua to GetAll.lua

--- GetAll.lua
+++ GetAll.lua
@@ -1,6 +1,7 @@
 -- This is an updated version of Merely's Recursion script. (https://github.com/MerelyRBLX/ROBLOX-Lua/blob/master/Recursion.lua)
+-- It uses the GetDescendants function so no recursion is needed.
 
-function recursion(obj, class, callback)
+function getAll(obj, class, callback)
 	for _, value in pairs(obj:GetDescendants()) do
 		if value:IsA(class) then
 			callback(value)
@@ -9,6 +10,6 @@ function recursion(obj, class, callback)
 end
 
 -- This anchors every BasePart in the Workspace.
-recursion(game.Workspace, "BasePart", function(obj)
+getAll(game.Workspace, "BasePart", function(obj)
 	obj.Anchored = true
 end)
